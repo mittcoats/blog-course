9fa6a09b76e0ed95a056934467b736b4571c34a1
Mitt Coats <mittcoats@gmail.com>
2016-02-09 20:45:14 -0500

    add edit and destory for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,6 +19,25 @@ class ProjectsController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @project.update project_params
+			redirect_to @project, notice: "Nice, save successful!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@project.destroy
+		redirect_to projects_path
+	end
+
 	private
 
 	def project_params
